326a160844e1abd222817abe8c03878fa039ef12
Chris <chrisfyi@hotmail.com>
2020-07-07 19:55:31 -0700

    Changes

--- server/db/client.js
+++ server/db/client.js
@@ -1,3 +1,5 @@
 const { Client } = require('pg'); 
 const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/afc-skate';
 const client = new Client(DATABASE_URL);
+
+module.exports = { client }
