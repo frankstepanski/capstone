d0ef591cfaad79c071be905d48a39c6f1a5af593
Frank Stepanski <fsjay@hotmail.com>
2020-07-10 20:02:33 -0400

    adding more front-end component files

--- client/App.js
+++ client/App.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import {  Route, Switch } from "react-router-dom";
 import { Home } from './pages/Home';
 import { Shop } from './pages/Shop';
 import { Blog } from './pages/Blog';
 import { Contact } from './pages/Contact';
 import { About } from './pages/About';
+import { Header } from './pages/layouts/Header'
 
 const App = () => {
     // useState
@@ -14,16 +15,17 @@ const App = () => {
     // useEffect
     
     return (
-      <>
-        <Switch>
-           <Route exact path = "/" component ={Home} />
-           <Route path = "/shop" component ={Shop} />
-           <Route path = "/shop" component ={Blog} />
-           <Route path = "/about" component ={About} />
-           <Route path = "/contact" component ={Contact} />
-           <Route component ={NoMatch} />
-        </Switch>
-      </>
+        <div className ="container">
+          <Header />
+           <Switch>
+              <Route exact path = "/" component ={Home} />
+              <Route path = "/shop" component ={Shop} />
+              <Route path = "/shop" component ={Blog} />
+              <Route path = "/about" component ={About} />
+              <Route path = "/contact" component ={Contact} />
+              <Route component ={NoMatch} />
+          </Switch>
+        </div>
     );
   };
 
--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { Nav, Navbar } from 'react-bootstrap';
 
-const NavigationBar = () => (
+const NavigationBar= () => (
 
     <Navbar expand="lg">
       <Navbar.Brand href="/">afc Skate</Navbar.Brand>
